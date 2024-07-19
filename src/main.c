2b350954aba8115c7ed958b15d5396d0de377cfb
Cafezinhu <aertoncastro@gmail.com>
2024-07-18 21:24:30 -0300

    bump difficulty

--- src/main.c
+++ src/main.c
@@ -21,7 +21,7 @@ const int screenHeight = 800;
 int rows = 10;
 int columns = 10;
 const float TILE_SIZE_CONSTANT = 600.0;
-int bombs_amt = 10;
+int bombs_amt = 15;
 int tiles_revealed = 0;
 bool lost = false;
 
